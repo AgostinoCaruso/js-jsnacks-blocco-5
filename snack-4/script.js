b1fe0394660ea9807c338c184b83489f0900f61f
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-15 15:33:20 +0100

    4 snack map & forEach

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,14 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+//come per il 3 io posso farlo sia con un foreach che con un map
+
+//forEach
+const authorName = [];
+posts.forEach((names) => authorName.push(names.author));
+//console.log(authorName);
+
+//map
+const authorNames2 = posts.map((names) => names.author);
+//console.log(authorNames2);
