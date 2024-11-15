6de8fd7b973da12fc00594469e65b077382a22dd
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-15 16:02:06 +0100

    snack 7 forEach

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+//qui devo soltanto recuperare un oggetto tramite una key e devo usare il find, oppure sempre il forEach
+
+let studentObj = [];
+students.forEach((student) => student.id === 2 ? studentObj = student : "non trovato");
+console.log(studentObj);
