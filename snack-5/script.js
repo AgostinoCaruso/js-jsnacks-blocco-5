c661a8edab6b278ee9ba37bc03f2dc74ab3eead1
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-15 15:39:18 +0100

    snack 5 with forEach

--- snack-5/script.js
+++ snack-5/script.js
@@ -5,3 +5,8 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 //essendo che qua NON vuole TUTTA l'array indietro non posso usare map, ma filter oppure sempre forEach, for of , for semplice
 
+//forEach
+const numsEven = [];
+nums.forEach((num) => num % 2 == 0 ? numsEven.push(num) : 0);
+
+console.log(numsEven);
