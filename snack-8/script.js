045f338cfdaecf018ba7f3eda6cb5ef964fcb46e
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-15 16:31:21 +0100

    snack 8 forEach, find and destructing

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,26 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+// qua come sempre uso o il find o forEach o anche una destrutturazione
+
+//forEach
+const classStudent = {};
+let classStudentString = "";   ////con stringa
+students.forEach((student) => student.name == "Marco Lanci" 
+//? classStudentString = student.class   ////con stringa
+? classStudent.class = student.class 
+: "non trovato");
+
+console.log(classStudent);
+//console.log(classStudentString);    ////con stringa
+
+//find
+const classStudent2 = students.find((student) => student.name == "Marco Lanci" ? student.class : "non trovato");
+
+console.log(classStudent2.class);
+
+//destrutturazione 
+const {class : studentClass } = students.find((nameStudent) => nameStudent.name =="Marco Lanci");
+console.log(studentClass);
+
